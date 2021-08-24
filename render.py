098a7575b726d9b9cf56dfc7828abded191c4a1b
J K Terry <justinkterry@gmail.com>
2021-08-24 18:24:24 -0400

    console logging

--- render.py
+++ render.py
@@ -49,6 +49,7 @@ for policy in policies:
             action = model.predict(observation, deterministic=True)[0] if not done else None
             render_env.step(action)
 
-        render_env.unwrapped.log_vortices('./mature_simulations/' + num + "_" + policy.split('.')[0] + 'vortices' + '.csv')
-        render_env.unwrapped.log_birds('./mature_simulations/' + num + "_" + policy.split('.')[0] + 'birds' + '.csv')
+        print('./mature_simulations/' + num + '_' + policy.split('.')[0] + 'vortices' + '.csv')
+        render_env.unwrapped.log_vortices('./mature_simulations/' + num + '_' + policy.split('.')[0] + 'vortices' + '.csv')
+        render_env.unwrapped.log_birds('./mature_simulations/' + num + '_' + policy.split('.')[0] + 'birds' + '.csv')
         break
