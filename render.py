81050fe652871829bc100f3c4d52664c6a03ea51
chorsch <22306897+chorsch@users.noreply.github.com>
2021-09-26 16:50:35 -0400

    Update render.py

--- render.py
+++ render.py
@@ -1,20 +1,20 @@
-from stable_baselines3 import PPO
-from pettingzoo.butterfly import pistonball_v4
-import supersuit as ss
-from stable_baselines3.common.vec_env import VecMonitor, VecTransposeImage, VecNormalize
-from stable_baselines3.common.evaluation import evaluate_policy
-from stable_baselines3.common.callbacks import EvalCallback
-from stable_baselines3.common.preprocessing import is_image_space, is_image_space_channels_first
-import numpy as np
 import os
 import sys
-from array2gif import write_gif
+from os.path import exists
+
 import fle.flocking_env as flocking_env
+import numpy as np
+import supersuit as ss
+from array2gif import write_gif
+from pettingzoo.butterfly import pistonball_v4
+from stable_baselines3 import PPO
+from stable_baselines3.common.callbacks import EvalCallback
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.preprocessing import is_image_space, is_image_space_channels_first
+from stable_baselines3.common.vec_env import VecMonitor, VecNormalize, VecTransposeImage
 
+#num = sys.argv[1]
 
-num = sys.argv[1]
-
-num = sys.argv[1]
 n_evaluations = 20
 n_agents = 9
 n_envs = 4
@@ -31,15 +31,25 @@ energy_reward_per_j = -10 / total_energy_j
 skip_frames = int(hz / nerve_impulse_hz)
 
 
-render_env = flocking_env.env(N=n_agents, h=1/hz, energy_reward=energy_reward_per_j, forward_reward=distance_reward_per_m, crash_reward=crash_reward, LIA=True)
+render_env = flocking_env.env(
+    N=n_agents,
+    h=1 / hz,
+    energy_reward=energy_reward_per_j,
+    forward_reward=distance_reward_per_m,
+    crash_reward=crash_reward,
+    LIA=True,
+)
 render_env = ss.delay_observations_v0(render_env, reaction_frames)
 render_env = ss.frame_skip_v0(render_env, skip_frames)
 
-policies = os.listdir('./mature_policies/' + str(num) + '/')
+policies = os.listdir("./optimization_policies/")
 
 for policy in policies:
-    print('Loading new policy')
-    model = PPO.load('./mature_policies/' + str(num) + '/' + policy)
+    filepath = "./optimization_policies/" + policy + "/best_model"
+    if not exists(filepath+'.zip'):
+        continue
+    print("Loading new policy ", filepath)
+    model = PPO.load(filepath)
 
     i = 0
     render_env.reset()
@@ -48,10 +58,11 @@ for policy in policies:
         for agent in render_env.agent_iter():
             observation, _, done, _ = render_env.last()
             action = model.predict(observation, deterministic=True)[0] if not done else None
+            print(action)
             render_env.step(action)
 
-        print('Saving vortex logs')
-        render_env.unwrapped.log_vortices('./mature_simulations/' + num + '_' + policy.split('.')[0] + '_vortices' + '.csv')
-        print('Saving bird logs')
-        render_env.unwrapped.log_birds('./mature_simulations/' + num + '_' + policy.split('.')[0] + '_birds' + '.csv')
+        print("Saving vortex logs")
+        render_env.unwrapped.log_vortices("./results/" + policy +"_vortices" + ".csv")
+        print("Saving bird logs")
+        render_env.unwrapped.log_birds("./results/" + policy + "_birds" + ".csv")
         break
