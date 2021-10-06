9291c09f980740514ef53ec683d52f526bbe1ced
J K Terry <justinkterry@gmail.com>
2021-10-05 22:49:22 -0400

    order change

--- run_render1.sh
+++ run_render1.sh
@@ -3,11 +3,11 @@ mkdir -p ./render_logs
 rm -rf mature_gifs/*
 rm -rf render_logs/*
 
-xvfb-run -a -s "-screen 0 1400x900x24" CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=1 nohup python3 render.py 0 &> ./render_logs/render_0.out &
-xvfb-run -a -s "-screen 0 1400x900x24" CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=1 nohup python3 render.py 1 &> ./render_logs/render_1.out &
-xvfb-run -a -s "-screen 0 1400x900x24" CUDA_VISIBLE_DEVICES=1 OMP_NUM_THREADS=1 nohup python3 render.py 2 &> ./render_logs/render_2.out &
-xvfb-run -a -s "-screen 0 1400x900x24" CUDA_VISIBLE_DEVICES=1 OMP_NUM_THREADS=1 nohup python3 render.py 3 &> ./render_logs/render_3.out &
-xvfb-run -a -s "-screen 0 1400x900x24" CUDA_VISIBLE_DEVICES=2 OMP_NUM_THREADS=1 nohup python3 render.py 4 &> ./render_logs/render_4.out &
-xvfb-run -a -s "-screen 0 1400x900x24" CUDA_VISIBLE_DEVICES=2 OMP_NUM_THREADS=1 nohup python3 render.py 5 &> ./render_logs/render_5.out &
-xvfb-run -a -s "-screen 0 1400x900x24" CUDA_VISIBLE_DEVICES=3 OMP_NUM_THREADS=1 nohup python3 render.py 6 &> ./render_logs/render_6.out &
-xvfb-run -a -s "-screen 0 1400x900x24" CUDA_VISIBLE_DEVICES=3 OMP_NUM_THREADS=1 nohup python3 render.py 7 &> ./render_logs/render_7.out &
+CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=1 xvfb-run -a -s "-screen 0 1400x900x24" nohup python3 render.py 0 &> ./render_logs/render_0.out &
+CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=1 xvfb-run -a -s "-screen 0 1400x900x24" nohup python3 render.py 1 &> ./render_logs/render_1.out &
+CUDA_VISIBLE_DEVICES=1 OMP_NUM_THREADS=1 xvfb-run -a -s "-screen 0 1400x900x24" nohup python3 render.py 2 &> ./render_logs/render_2.out &
+CUDA_VISIBLE_DEVICES=1 OMP_NUM_THREADS=1 xvfb-run -a -s "-screen 0 1400x900x24" nohup python3 render.py 3 &> ./render_logs/render_3.out &
+CUDA_VISIBLE_DEVICES=2 OMP_NUM_THREADS=1 xvfb-run -a -s "-screen 0 1400x900x24" nohup python3 render.py 4 &> ./render_logs/render_4.out &
+CUDA_VISIBLE_DEVICES=2 OMP_NUM_THREADS=1 xvfb-run -a -s "-screen 0 1400x900x24" nohup python3 render.py 5 &> ./render_logs/render_5.out &
+CUDA_VISIBLE_DEVICES=3 OMP_NUM_THREADS=1 xvfb-run -a -s "-screen 0 1400x900x24" nohup python3 render.py 6 &> ./render_logs/render_6.out &
+CUDA_VISIBLE_DEVICES=3 OMP_NUM_THREADS=1 xvfb-run -a -s "-screen 0 1400x900x24" nohup python3 render.py 7 &> ./render_logs/render_7.out &
