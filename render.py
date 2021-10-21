0284db683adc216e443a0a371bae828b793d0aa0
J K Terry <justinkterry@gmail.com>
2021-10-21 14:40:23 -0400

    remove junk code

--- render.py
+++ render.py
@@ -57,20 +57,3 @@ for policy in policies:
     write_gif(
         obs_list, "./mature_gifs/" + num + "_" + policy.split(".")[0] + ".gif", fps=50
     )
-
-
-import gym
-
-env = gym.make('CartPole-v0')
-env.monitor.start('/tmp/cartpole-experiment-1', force=True)
-observation = env.reset()
-for t in range(100):
-#    env.render()
-    print(observation)
-    action = env.action_space.sample()
-    observation, reward, done, info = env.step(action)
-    if done:
-        print("Episode finished after {} timesteps".format(t+1))
-        break
-
-env.monitor.close()
