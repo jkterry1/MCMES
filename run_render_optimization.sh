b2fc423dc3498397cb336d09ff7f1f837c18385a
J K Terry <justinkterry@gmail.com>
2021-11-19 22:38:10 -0500

    typo

--- run_render_optimization.sh
+++ run_render_optimization.sh
@@ -1,4 +1,4 @@
 mkdir -p ./optimization_gifs
 rm -rf optimization_gifs/*
 
-python3 nohup render_optimization_policies.py &> render_optimization_log.out &
+nohup python3 render_optimization_policies.py &> render_optimization_log.out &
