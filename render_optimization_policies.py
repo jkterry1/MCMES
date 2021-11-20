3ec5b11330b7faaf2b7ae66c0cb553d374f3fd62
J K Terry <justinkterry@gmail.com>
2021-11-19 22:41:15 -0500

    remove error catching

--- render_optimization_policies.py
+++ render_optimization_policies.py
@@ -25,27 +25,24 @@ for policy in policies:
     env.reset()
     reward = 0
 
-    try:
-        while True:
-            for agent in env.agent_iter():
-                observation, _, done, reward = env.last()
-                action = (model.predict(observation, deterministic=True)[0] if not done else None)
-                reward += reward
+    while True:
+        for agent in env.agent_iter():
+            observation, _, done, reward = env.last()
+            action = (model.predict(observation, deterministic=True)[0] if not done else None)
+            reward += reward
 
-                reward = reward / n_agents
+            reward = reward / n_agents
 
-                env.step(action)
-                i += 1
-                if i % (len(env.possible_agents) + 1) == 0:
-                    obs_list.append(
-                        np.transpose(env.render(mode="rgb_array"), axes=(1, 0, 2))
-                    )
+            env.step(action)
+            i += 1
+            if i % (len(env.possible_agents) + 1) == 0:
+                obs_list.append(
+                    np.transpose(env.render(mode="rgb_array"), axes=(1, 0, 2))
+                )
 
-            break
+        break
 
-        print("writing gif")
-        write_gif(
-            obs_list, "./optimization_gifs/" + policy + "_" + "reward" + ".gif", fps=5
-        )
-    except:
-        print("error")
+    print("writing gif")
+    write_gif(
+        obs_list, "./optimization_gifs/" + policy + "_" + "reward" + ".gif", fps=5
+    )
