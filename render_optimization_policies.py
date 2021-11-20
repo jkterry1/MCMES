c92d53bade032178253e76beb0c4fffae287dd48
J K Terry <justinkterry@gmail.com>
2021-11-20 01:02:38 -0500

    nohup and friends

--- render_optimization_policies.py
+++ render_optimization_policies.py
@@ -25,23 +25,26 @@ for policy in policies:
     env.reset()
     reward = 0
 
-    while True:
-        for agent in env.agent_iter():
-            observation, reward, done, _ = env.last()
-            action = (model.predict(observation, deterministic=True)[0] if not done else None)
-            reward += reward
+    try:
+        while True:
+            for agent in env.agent_iter():
+                observation, reward, done, _ = env.last()
+                action = (model.predict(observation, deterministic=True)[0] if not done else None)
+                reward += reward
 
-            env.step(action)
-            i += 1
-            if i % (len(env.possible_agents) + 1) == 0:
-                obs_list.append(
-                    np.transpose(env.render(mode="rgb_array"), axes=(1, 0, 2))
-                )
+                env.step(action)
+                i += 1
+                if i % (len(env.possible_agents) + 1) == 0:
+                    obs_list.append(
+                        np.transpose(env.render(mode="rgb_array"), axes=(1, 0, 2))
+                    )
 
-        break
+            break
 
-    reward = reward / n_agents
-    print("writing gif")
-    write_gif(
-        obs_list, "./optimization_gifs/" + policy + "_" + "reward" + ".gif", fps=5
-    )
+        reward = reward / n_agents
+        print("writing gif")
+        write_gif(
+            obs_list, "./optimization_gifs/" + policy + "_" + "reward" + ".gif", fps=5
+        )
+    except:
+        print("error")
