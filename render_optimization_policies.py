144df12b28a0b04dcaf83469b80a0d47fd3d21a9
J K Terry <justinkterry@gmail.com>
2021-11-29 16:53:22 -0500

    fix

--- render_optimization_policies.py
+++ render_optimization_policies.py
@@ -10,7 +10,7 @@ env = sumo_rl.ingolstadt7(sumo_warnings=False)
 env = ss.pad_observations_v0(env)
 env = ss.pad_action_space_v0(env)
 
-n_agents = 8
+n_agents = 7
 
 policies = os.listdir("./optimization_policies/")
 
@@ -26,24 +26,26 @@ for policy in policies:
     env.reset()
     reward = 0
 
-    while True:
-        for agent in env.agent_iter():
-            observation, _, done, reward = env.last()
-            action = (model.predict(observation, deterministic=True)[0] if not done else None)
-            reward += reward
+    try:
+        while True:
+            for agent in env.agent_iter():
+                observation, reward, done, _ = env.last()
+                action = (model.predict(observation, deterministic=False)[0] if not done else None)
+                reward += reward
 
-            reward = reward / n_agents
+                env.step(action)
+                i += 1
+                if i % (len(env.possible_agents) + 1) == 0:
+                    obs_list.append(
+                        np.transpose(env.render(mode="rgb_array"), axes=(1, 0, 2))
+                    )
 
-            env.step(action)
-            i += 1
-            if i % (len(env.possible_agents) + 1) == 0:
-                obs_list.append(
-                    np.transpose(env.render(mode="rgb_array"), axes=(1, 0, 2))
-                )
+            break
 
-        break
-
-    print("writing gif")
-    write_gif(
-        obs_list, "./optimization_gifs/" + policy + "_" + "reward" + ".gif", fps=50
-    )
+        reward = reward / n_agents
+        print("writing gif")
+        write_gif(
+            obs_list, "./optimization_gifs/" + policy + "_" + str(reward)[:5] + ".gif", fps=5
+        )
+    except:
+        print("error")
