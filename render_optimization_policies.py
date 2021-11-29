b99d7a78af5c3c32757b0de8fb045cc88f4e2536
J K Terry <justinkterry@gmail.com>
2021-11-29 16:57:01 -0500

    fix

--- render_optimization_policies.py
+++ render_optimization_policies.py
@@ -7,14 +7,11 @@ from pettingzoo.butterfly import pistonball_v4
 from stable_baselines3 import PPO
 
 n_agents = 20
-n_envs = 4
 
-env = pistonball_v4.parallel_env(continuous=False)
+env = pistonball_v4.env(continuous=False)
 env = ss.color_reduction_v0(env, mode="B")
 env = ss.resize_v0(env, x_size=84, y_size=84)
 env = ss.frame_stack_v1(env, 3)
-env = ss.pettingzoo_env_to_vec_env_v0(env)
-env = ss.concat_vec_envs_v0(env, n_envs, num_cpus=1, base_class="stable_baselines3")
 
 policies = os.listdir("./optimization_policies/")
 
@@ -30,24 +27,26 @@ for policy in policies:
     env.reset()
     reward = 0
 
-    while True:
-        for agent in env.agent_iter():
-            observation, _, done, reward = env.last()
-            action = (model.predict(observation, deterministic=True)[0] if not done else None)
-            reward += reward
+    try:
+        while True:
+            for agent in env.agent_iter():
+                observation, reward, done, _ = env.last()
+                action = (model.predict(observation, deterministic=True)[0] if not done else None)
+                reward += reward
 
-            reward = reward / n_agents
+                env.step(action)
+                i += 1
+                if i % (len(env.possible_agents) + 1) == 0:
+                    obs_list.append(
+                        np.transpose(env.render(mode="rgb_array"), axes=(1, 0, 2))
+                    )
 
-            env.step(action)
-            i += 1
-            if i % (len(env.possible_agents) + 1) == 0:
-                obs_list.append(
-                    np.transpose(env.render(mode="rgb_array"), axes=(1, 0, 2))
-                )
+            break
 
-        break
-
-    print("writing gif")
-    write_gif(
-        obs_list, "./optimization_gifs/" + policy + "_" + "reward" + ".gif", fps=5
-    )
+        reward = reward / n_agents
+        print("writing gif")
+        write_gif(
+            obs_list, "./optimization_gifs/" + policy + "_" + str(reward)[:5] + ".gif", fps=15
+        )
+    except:
+        print("error")
