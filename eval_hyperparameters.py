fdc0655e68a0de0647165b7d60da57fed52f0fdb
J K Terry <justinkterry@gmail.com>
2021-11-29 18:43:02 -0500

    fix line

--- eval_hyperparameters.py
+++ eval_hyperparameters.py
@@ -82,7 +82,7 @@ for i in range(10):
         print(mean_reward)
         print(std_reward)
         all_mean_rewards.append(mean_reward)
-        if mean_reward > 10:
+        if mean_reward > -.1:
             model.save(
                 "./mature_policies/"
                 + str(num)
