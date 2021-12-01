6b6ed53bc0f7823e80463a632f2728bae2a74b28
J K Terry <justinkterry@gmail.com>
2021-12-01 16:00:59 -0500

    pdb

--- eval_hyperparameters.py
+++ eval_hyperparameters.py
@@ -11,6 +11,7 @@ from stable_baselines3.common.preprocessing import (
 )
 from torch import nn as nn
 import sumo_rl
+import pdb
 
 num = sys.argv[1]
 n_evaluations = 20
@@ -77,6 +78,7 @@ for i in range(10):
             render=False,
         )
         print('c')
+        breakpoint()
         model.learn(total_timesteps=n_timesteps, callback=eval_callback)
         print('d')
         model = PPO.load("./eval_logs/" + num + "/" + str(i) + "/" + "best_model")
