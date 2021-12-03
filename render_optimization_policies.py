06e7efe4b7e33f9dd1678f1f81147a5d6c6d94ae
J K Terry <justinkterry@gmail.com>
2021-12-03 14:57:45 -0500

    fix

--- render_optimization_policies.py
+++ render_optimization_policies.py
@@ -5,16 +5,16 @@ import supersuit as ss
 from array2gif import write_gif
 from pettingzoo.sisl import pursuit_v4
 from stable_baselines3 import PPO
-from stable_baselines3.common.preprocessing import is_image_space, is_image_space_channels_first
-from stable_baselines3.common.vec_env import VecMonitor, VecTransposeImage,
+# from stable_baselines3.common.preprocessing import is_image_space, is_image_space_channels_first
+# from stable_baselines3.common.vec_env import VecTransposeImage
 
 
-def image_transpose(env):
-    if is_image_space(env.observation_space) and not is_image_space_channels_first(
-        env.observation_space
-    ):
-        env = VecTransposeImage(env)
-    return env
+# def image_transpose(env):
+#     if is_image_space(env.observation_space) and not is_image_space_channels_first(
+#         env.observation_space
+#     ):
+#         env = VecTransposeImage(env)
+#     return env
 
 
 env = pursuit_v4.env()
