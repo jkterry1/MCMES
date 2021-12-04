2328a29f56058e4923286592db189f9df5489e22
J K Terry <justinkterry@gmail.com>
2021-12-03 21:26:25 -0500

    split eval

--- run_eval2.sh
+++ run_eval2.sh
@@ -10,11 +10,3 @@ sleep 3
 CUDA_VISIBLE_DEVICES=1 OMP_NUM_THREADS=1 nohup python3 eval_hyperparameters.py 10 &> ./eval_logs/eval_10.out &
 sleep 3
 CUDA_VISIBLE_DEVICES=1 OMP_NUM_THREADS=1 nohup python3 eval_hyperparameters.py 11 &> ./eval_logs/eval_11.out &
-sleep 3
-CUDA_VISIBLE_DEVICES=2 OMP_NUM_THREADS=1 nohup python3 eval_hyperparameters.py 12 &> ./eval_logs/eval_12.out &
-sleep 3
-CUDA_VISIBLE_DEVICES=2 OMP_NUM_THREADS=1 nohup python3 eval_hyperparameters.py 13 &> ./eval_logs/eval_13.out &
-sleep 3
-CUDA_VISIBLE_DEVICES=3 OMP_NUM_THREADS=1 nohup python3 eval_hyperparameters.py 14 &> ./eval_logs/eval_14.out &
-sleep 3
-CUDA_VISIBLE_DEVICES=3 OMP_NUM_THREADS=1 nohup python3 eval_hyperparameters.py 15 &> ./eval_logs/eval_15.out &
