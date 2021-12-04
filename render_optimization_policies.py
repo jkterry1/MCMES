f30eee301e6ddaf7cdf3cc3f0761efef1ca97c13
J K Terry <justinkterry@gmail.com>
2021-12-03 21:44:53 -0500

    remove try except

--- render_optimization_policies.py
+++ render_optimization_policies.py
@@ -32,26 +32,23 @@ for policy in policies:
     env.reset()
     total_reward = 0
 
-    try:
-        while True:
-            for agent in env.agent_iter():
-                observation, reward, done, _ = env.last()
-                action = (model.predict(observation, deterministic=True)[0] if not done else None)
-                total_reward += reward
+    while True:
+        for agent in env.agent_iter():
+            observation, reward, done, _ = env.last()
+            action = (model.predict(observation, deterministic=True)[0] if not done else None)
+            total_reward += reward
 
-                env.step(action)
-                i += 1
-                if i % (len(env.possible_agents) + 1) == 0:
-                    obs_list.append(
-                        np.transpose(env.render(mode="rgb_array"), axes=(1, 0, 2))
-                    )
+            env.step(action)
+            i += 1
+            if i % (len(env.possible_agents) + 1) == 0:
+                obs_list.append(
+                    np.transpose(env.render(mode="rgb_array"), axes=(1, 0, 2))
+                )
 
-            break
+        break
 
-        total_reward = total_reward / n_agents
-        print("writing gif")
-        write_gif(
-            obs_list, "./optimization_gifs/" + policy + "_" + str(total_reward)[:5] + ".gif", fps=50
-        )
-    except:
-        print("error")
+    total_reward = total_reward / n_agents
+    print("writing gif")
+    write_gif(
+        obs_list, "./optimization_gifs/" + policy + "_" + str(total_reward)[:5] + ".gif", fps=50
+    )
