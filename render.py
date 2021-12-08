5d9b9c0035cd66e79da63477ba0f5f705e9b1e11
J K Terry <justinkterry@gmail.com>
2021-12-08 00:42:48 -0500

    updates to render

--- render.py
+++ render.py
@@ -16,8 +16,8 @@ num = sys.argv[1]
 
 n_agents = 9
 n_envs = 4
-total_energy_j = 23698
-total_distance_m = 870
+total_energy_j = 24212
+total_distance_m = 894
 hz = 500
 crash_reward = -10
 episodes = 300
--- render_optimization_policies.py
+++ render_optimization_policies.py
@@ -15,8 +15,8 @@ from stable_baselines3.common.vec_env import VecMonitor, VecNormalize, VecTransp
 
 n_agents = 9
 n_envs = 4
-total_energy_j = 23698
-total_distance_m = 870
+total_energy_j = 24212
+total_distance_m = 894
 hz = 500
 crash_reward = -10
 episodes = 300
