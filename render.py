fbcbe4a5c05711bba6a708d148193a454c9761ed
J K Terry <justinkterry@gmail.com>
2021-12-13 17:06:50 -0500

    start 5

--- render.py
+++ render.py
@@ -33,27 +33,28 @@ policies = os.listdir("./mature_policies/" + str(num) + "/")
 for policy in policies:
     model = PPO.load("./mature_policies/" + str(num) + "/" + policy)
 
-    obs_list = []
-    i = 0
-    env.reset()
+    for j in ['a','b','c','d','e']:
+        obs_list = []
+        i = 0
+        env.reset()
 
-    while True:
-        for agent in env.agent_iter():
-            observation, _, done, _ = env.last()
-            action = (
-                model.predict(observation, deterministic=True)[0] if not done else None
-            )
-
-            env.step(action)
-            i += 1
-            if i % (len(env.possible_agents) + 1) == 0:
-                obs_list.append(
-                    np.transpose(env.render(mode="rgb_array"), axes=(1, 0, 2))
+        while True:
+            for agent in env.agent_iter():
+                observation, _, done, _ = env.last()
+                action = (
+                    model.predict(observation, deterministic=True)[0] if not done else None
                 )
-        env.close()
-        break
 
-    print("writing gif")
-    write_gif(
-        obs_list, "./mature_gifs/" + num + "_" + policy.split(".")[0] + ".gif", fps=15
-    )
+                env.step(action)
+                i += 1
+                if i % (len(env.possible_agents) + 1) == 0:
+                    obs_list.append(
+                        np.transpose(env.render(mode="rgb_array"), axes=(1, 0, 2))
+                    )
+            env.close()
+            break
+
+        print("writing gif")
+        write_gif(
+            obs_list, "./mature_gifs/" + num + "_" + policy.split(".")[0] + ".gif", fps=15
+        )
