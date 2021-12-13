b9978522ef288125e212c167812e9fcae669f33c
J K Terry <justinkterry@gmail.com>
2021-12-13 18:58:16 -0500

    red line, 5 trials, proper reward naming

--- render.py
+++ render.py
@@ -20,34 +20,41 @@ num = sys.argv[1]
 env = pursuit_v4.env()
 env = ss.flatten_v0(env)
 
+n_agents = 8
+
 policies = os.listdir("./mature_policies/" + str(num) + "/")
 
 for policy in policies:
     model = PPO.load("./mature_policies/" + str(num) + "/" + policy)
 
-    obs_list = []
-    i = 0
-    env.reset()
+    for j in ['a','b','c','d','e']:
 
-    while True:
-        for agent in env.agent_iter():
-            observation, _, done, _ = env.last()
-            action = (
-                model.predict(observation, deterministic=False)[0] if not done else None
+        obs_list = []
+        i = 0
+        env.reset()
+        total_reward = 0
+
+        while True:
+            for agent in env.agent_iter():
+                observation, reward, done, _ = env.last()
+                action = (model.predict(observation, deterministic=False)[0] if not done else None)
+                total_reward += reward
+
+                env.step(action)
+                i += 1
+                if i % (len(env.possible_agents) + 1) == 0:
+                    obs_list.append(
+                        np.transpose(env.render(mode="rgb_array"), axes=(1, 0, 2))
+                    )
+
+            break
+
+        total_reward = total_reward / n_agents
+
+        if total_reward > 50:
+            print("writing gif")
+            write_gif(
+                obs_list, "./mature_gifs/" + num + "_" + policy.split("_")[0] + j + '_' + total_reward + ".gif", fps=10
             )
-
-            env.step(action)
-            i += 1
-            if i % (len(env.possible_agents) + 1) == 0:
-                obs_list.append(
-                    np.transpose(env.render(mode="rgb_array"), axes=(1, 0, 2))
-                )
-
-        break
-
-    print("writing gif")
-    write_gif(
-        obs_list, "./mature_gifs/" + num + "_" + policy.split(".")[0] + ".gif", fps=10
-    )
 
 env.close()
