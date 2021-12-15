1a25294df584966e6dadffe229f771674f75e20e
J K Terry <justinkterry@gmail.com>
2021-12-15 12:46:52 -0500

    5x

--- render.py
+++ render.py
@@ -19,7 +19,9 @@ num = sys.argv[1]
 #         env = VecTransposeImage(env)
 #     return env
 
-RESOLUTION = (5000, 5000)
+RESOLUTION = (4000, 4000)
+
+n_agents = 7
 
 env = sumo_rl.ingolstadt7(sumo_warnings=False, virtual_display=RESOLUTION)
 env = from_parallel(env)
@@ -31,27 +33,34 @@ policies = os.listdir("./mature_policies/" + str(num) + "/")
 for policy in policies:
     model = PPO.load("./mature_policies/" + str(num) + "/" + policy)
 
-    obs_list = []
-    i = 0
-    env.reset()
+    for j in ['a','b','c','d','e']:
 
-    while True:
-        for agent in env.agent_iter():
-            observation, _, done, _ = env.last()
-            action = (
-                model.predict(observation, deterministic=True)[0] if not done else None
+        obs_list = []
+        i = 0
+        env.reset()
+        total_reward = 0
+
+        while True:
+            for agent in env.agent_iter():
+                observation, reward, done, _ = env.last()
+                action = (model.predict(observation, deterministic=True)[0] if not done else None)
+                total_reward += reward
+
+                env.step(action)
+                i += 1
+                if i % (len(env.possible_agents) + 1) == 0:
+                    obs_list.append(
+                        np.transpose(env.render(mode="rgb_array"), axes=(1, 0, 2))
+                    )
+
+            break
+
+        total_reward = total_reward / n_agents
+
+        if total_reward > -.1:
+            print("writing gif")
+            write_gif(
+                obs_list, "./mature_gifs/" + num + "_" + policy.split("_")[0] + j + '_' + str(total_reward)[:5] + ".gif", fps=50
             )
 
-            env.step(action)
-            i += 1
-            if i % (len(env.possible_agents) + 1) == 0:
-                obs_list.append(
-                    np.transpose(env.render(mode="rgb_array"), axes=(1, 0, 2))
-                )
-        env.close()
-        break
-
-    print("writing gif")
-    write_gif(
-        obs_list, "./mature_gifs/" + num + "_" + policy.split(".")[0] + ".gif", fps=50
-    )
+env.close()
