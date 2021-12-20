e91ceb54a47fbfa9c9c0f60839dd21f524b7d46b
J K Terry <justinkterry@gmail.com>
2021-12-20 02:18:40 -0500

    try to make render work

--- render.py
+++ render.py
@@ -1,4 +1,4 @@
-from stable_baselines3 import PPO
+from stable_baselines3 import DQN
 from pettingzoo.butterfly import pistonball_v5
 import supersuit as ss
 from stable_baselines3.common.vec_env import VecMonitor, VecTransposeImage, VecNormalize
@@ -22,6 +22,7 @@ num = sys.argv[1]
 #         env = VecTransposeImage(env)
 #     return env
 
+n_agents = 20
 
 env = pistonball_v5.env(continuous=False)
 env = ss.color_reduction_v0(env, mode="B")
@@ -31,29 +32,34 @@ env = ss.frame_stack_v1(env, 3)
 policies = os.listdir("./mature_policies/" + str(num) + "/")
 
 for policy in policies:
-    model = PPO.load("./mature_policies/" + str(num) + "/" + policy)
+    model = DQN.load("./mature_policies/" + str(num) + "/" + policy)
 
-    obs_list = []
-    i = 0
-    env.reset()
+    for j in ['a','b','c','d','e']:
 
-    while True:
-        for agent in env.agent_iter():
-            observation, _, done, _ = env.last()
-            action = (
-                model.predict(observation, deterministic=True)[0] if not done else None
+        obs_list = []
+        i = 0
+        env.reset()
+        total_reward = 0
+
+        while True:
+            for agent in env.agent_iter():
+                observation, reward, done, _ = env.last()
+                action = (model.predict(observation, deterministic=True)[0] if not done else None)
+                total_reward += reward
+
+                env.step(action)
+                i += 1
+                if i % (len(env.possible_agents) + 1) == 0:
+                    obs_list.append(
+                        np.transpose(env.render(mode="rgb_array"), axes=(1, 0, 2))
+                    )
+
+            break
+
+        total_reward = total_reward / n_agents
+
+        if total_reward > 90:
+            print("writing gif")
+            write_gif(
+                obs_list, "./mature_gifs/" + num + "_" + policy.split("_")[0] + j + '_' + str(total_reward)[:5] + ".gif", fps=15
             )
-
-            env.step(action)
-            i += 1
-            if i % (len(env.possible_agents) + 1) == 0:
-                obs_list.append(
-                    np.transpose(env.render(mode="rgb_array"), axes=(1, 0, 2))
-                )
-        env.close()
-        break
-
-    print("writing gif")
-    write_gif(
-        obs_list, "./mature_gifs/" + num + "_" + policy.split(".")[0] + ".gif", fps=15
-    )
