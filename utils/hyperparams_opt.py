27603843033a34c7404c99789a6e5c81b411d419
J K Terry <jkterry0@gmail.com>
2022-02-25 09:46:33 -0500

    Update hyperparams_opt.py

--- utils/hyperparams_opt.py
+++ utils/hyperparams_opt.py
@@ -17,7 +17,7 @@ def sample_ppo_params(trial: optuna.Trial) -> Dict[str, Any]:
     """
     batch_size = trial.suggest_categorical("batch_size", [16, 32, 64, 128, 256, 512])
     n_steps = trial.suggest_categorical("n_steps", [32, 64, 128, 256, 512])
-    gamma = trial.suggest_categorical("gamma", [0.9, 0.95, 0.98, 0.99, 0.995, 0.999, 0.9999])
+    gamma = trial.suggest_categorical("gamma", [0.995, 0.999, 0.9999, 0.99999])
     learning_rate = trial.suggest_loguniform("learning_rate", 1e-5, 1)
     lr_schedule = "constant"
     # Uncomment to enable learning rate schedule
