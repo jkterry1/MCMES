15fee8e7423e5e51b24615f15ab6806f2041215b
J K Terry <jkterry0@gmail.com>
2022-03-04 21:21:56 -0500

    comment out breakpoint

--- utils/exp_manager.py
+++ utils/exp_manager.py
@@ -742,7 +742,7 @@ class ExperimentManager(object):
         )
 
         try:
-            breakpoint()
+            # breakpoint()
             study.optimize(self.objective, n_trials=self.n_trials, n_jobs=self.n_jobs)
         except KeyboardInterrupt:
             pass
