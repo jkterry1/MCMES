2a28109698344a77c497f2f540dc2e05949e1979
J K Terry <jkterry0@gmail.com>
2022-03-07 14:04:17 -0500

    verbose logging

--- utils/exp_manager.py
+++ utils/exp_manager.py
@@ -594,7 +594,7 @@ class ExperimentManager(object):
             tensorboard_log=None,
             # We do not seed the trial
             seed=None,
-            verbose=0,
+            verbose=self.verbose,
             **kwargs,
         )
 
