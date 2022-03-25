c6b30cbb67908c2091368893ef07a135e3cddeef
snow-fox <taijunjet@hotmail.com>
2022-03-25 03:21:28 +0000

    remove redundant rm rf

--- run_optimize.sh
+++ run_optimize.sh
@@ -5,9 +5,6 @@ rm -rf optimize_logs/*
 mkdir -p ./optimization_policies
 rm -rf optimization_policies/*
 
-mkdir -p ./optimization_policies
-rm -rf optimization_policies/*
-
 CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=1 nohup python3 train.py --algo ppo --env LunarLanderContinuous-v2 -n 27000000 -optimize --optimization-log-path optimization_policies --n-trials 150 --sampler tpe --pruner median --study-name $1 --storage mysql://database:ZrWdchqeNpmbuAXYpy2V@35.194.57.226/$1 --verbose 2 &> ./optimize_logs/optimize_0.out &
 sleep 3
 CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=1 nohup python3 train.py --algo ppo --env LunarLanderContinuous-v2 -n 27000000 -optimize --optimization-log-path optimization_policies --n-trials 150 --sampler tpe --pruner median --study-name $1 --storage mysql://database:ZrWdchqeNpmbuAXYpy2V@35.194.57.226/$1 --verbose 2 &> ./optimize_logs/optimize_1.out &
