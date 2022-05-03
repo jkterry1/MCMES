edf5fad90de6180e52bf4db244b92bbcaf25c101
Jet <taijunjet@hotmail.com>
2022-05-03 17:44:47 +0100

    add hacky optuna

---
+++ hacky_optuna.py
@@ -0,0 +1,27 @@
+import argparse
+import json
+
+import optuna
+
+
+def value_key(a):
+    if a.value is None:
+        return float("-inf")
+    else:
+        return a.value
+
+study = optuna.create_study(
+    study_name=None,
+    storage='mysql://database:ZrWdchqeNpmbuAXYpy2V@35.194.57.226',
+    load_if_exists=True,
+    direction="maximize",
+)
+values = []
+trials = study.trials
+trials.sort(key=value_key, reverse=True)
+
+print(str(len(trials)) + '\n')
+
+for trial in trials:
+    if len(values) < 5:
+        print(trial)
