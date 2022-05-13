d0a9d6cb1e8aee529dc6d9b2ec11e15f2503bb18
Jet <taijunjet@hotmail.com>
2022-05-13 15:19:20 +0100

    ignore nohup

--- run_single_render.sh
+++ run_single_render.sh
@@ -6,4 +6,4 @@ mkdir -p ./results
 rm -rf render_optimization_logs/*
 mkdir -p ./render_optimization_logs
 
-OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=0 nohup python3 render_optimization_policies.py $1
+OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=0 python3 render_optimization_policies.py $1
