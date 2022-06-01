845675fa29dd968d0c0bc2d7aaefa7d99756ce03
J K Terry <jkterry0@gmail.com>
2022-06-01 13:54:25 -0400

    cleanup

--- render_optimization_policies.py
+++ render_optimization_policies.py
@@ -1,10 +1,11 @@
 import os
 from os.path import exists
+
 import numpy as np
+import sumo_rl
 import supersuit as ss
 from array2gif import write_gif
 from stable_baselines3 import PPO
-import sumo_rl
 
 env = sumo_rl.ingolstadt21(sumo_warnings=False)
 env = ss.pad_observations_v0(env)
@@ -16,7 +17,7 @@ policies = os.listdir("./optimization_policies/")
 
 for policy in policies:
     filepath = "./optimization_policies/" + policy + "/best_model"
-    if not exists(filepath + '.zip'):
+    if not exists(filepath + ".zip"):
         continue
     print("Loading new policy ", filepath)
     model = PPO.load(filepath)
@@ -30,22 +31,18 @@ for policy in policies:
         while True:
             for agent in env.agent_iter():
                 observation, reward, done, _ = env.last()
-                action = (model.predict(observation, deterministic=True)[0] if not done else None)
+                action = model.predict(observation, deterministic=True)[0] if not done else None
                 reward += reward
 
                 env.step(action)
                 i += 1
                 if i % (len(env.possible_agents) + 1) == 0:
-                    obs_list.append(
-                        np.transpose(env.render(mode="rgb_array"), axes=(1, 0, 2))
-                    )
+                    obs_list.append(np.transpose(env.render(mode="rgb_array"), axes=(1, 0, 2)))
 
             break
 
         reward = reward / n_agents
         print("writing gif")
-        write_gif(
-            obs_list, "./optimization_gifs/" + policy + "_" + str(reward)[:5] + ".gif", fps=5
-        )
+        write_gif(obs_list, "./optimization_gifs/" + policy + "_" + str(reward)[:5] + ".gif", fps=5)
     except:
         print("error")
