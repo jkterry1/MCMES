ade28f91aec3e7a5210a228f5240d0054d18e8ba
J K Terry <jkterry0@gmail.com>
2022-06-01 14:02:12 -0400

    fix

--- render_optimization_policies.py
+++ render_optimization_policies.py
@@ -1,5 +1,6 @@
 import os
 from os.path import exists
+
 import numpy as np
 import supersuit as ss
 from array2gif import write_gif
@@ -17,7 +18,7 @@ policies = os.listdir("./optimization_policies/")
 
 for policy in policies:
     filepath = "./optimization_policies/" + policy + "/best_model"
-    if not exists(filepath + '.zip'):
+    if not exists(filepath + ".zip"):
         continue
     print("Loading new policy ", filepath)
     model = PPO.load(filepath)
@@ -31,22 +32,18 @@ for policy in policies:
         while True:
             for agent in env.agent_iter():
                 observation, reward, done, _ = env.last()
-                action = (model.predict(observation, deterministic=True)[0] if not done else None)
+                action = model.predict(observation, deterministic=True)[0] if not done else None
                 total_reward += reward
 
                 env.step(action)
                 i += 1
                 if i % (len(env.possible_agents) + 1) == 0:
-                    obs_list.append(
-                        np.transpose(env.render(mode="rgb_array"), axes=(1, 0, 2))
-                    )
+                    obs_list.append(np.transpose(env.render(mode="rgb_array"), axes=(1, 0, 2)))
 
             break
 
         total_reward = total_reward / n_agents
         print("writing gif")
-        write_gif(
-            obs_list, "./optimization_gifs/" + policy + "_" + str(total_reward)[:5] + ".gif", fps=15
-        )
+        write_gif(obs_list, "./optimization_gifs/" + policy + "_" + str(total_reward)[:5] + ".gif", fps=15)
     except:
         print("error")
