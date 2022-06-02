130712a1f556962b2858fe4a3b1925087329b364
J K Terry <jkterry0@gmail.com>
2022-06-02 12:23:26 -0400

    lint

--- eval_hyperparameters.py
+++ eval_hyperparameters.py
@@ -1,15 +1,13 @@
-import sys
 import json
-from stable_baselines3 import PPO
-from pettingzoo.butterfly import pistonball_v6
+import sys
+
 import supersuit as ss
-from stable_baselines3.common.vec_env import VecMonitor, VecTransposeImage, VecNormalize
-from stable_baselines3.common.evaluation import evaluate_policy
+from pettingzoo.butterfly import pistonball_v6
+from stable_baselines3 import PPO
 from stable_baselines3.common.callbacks import EvalCallback
-from stable_baselines3.common.preprocessing import (
-    is_image_space,
-    is_image_space_channels_first,
-)
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.preprocessing import is_image_space, is_image_space_channels_first
+from stable_baselines3.common.vec_env import VecMonitor, VecTransposeImage
 
 num = sys.argv[1]
 n_evaluations = 20
@@ -24,9 +22,7 @@ print(params)
 
 
 def image_transpose(env):
-    if is_image_space(env.observation_space) and not is_image_space_channels_first(
-        env.observation_space
-    ):
+    if is_image_space(env.observation_space) and not is_image_space_channels_first(env.observation_space):
         env = VecTransposeImage(env)
     return env
 
@@ -45,9 +41,7 @@ eval_env = ss.color_reduction_v0(eval_env, mode="B")
 eval_env = ss.resize_v0(eval_env, x_size=84, y_size=84)
 eval_env = ss.frame_stack_v1(eval_env, 3)
 eval_env = ss.pettingzoo_env_to_vec_env_v1(eval_env)
-eval_env = ss.concat_vec_envs_v1(
-    eval_env, 1, num_cpus=1, base_class="stable_baselines3"
-)
+eval_env = ss.concat_vec_envs_v1(eval_env, 1, num_cpus=1, base_class="stable_baselines3")
 eval_env = VecMonitor(eval_env)
 eval_env = image_transpose(eval_env)
 
@@ -69,22 +63,12 @@ for i in range(10):
         )
         model.learn(total_timesteps=n_timesteps, callback=eval_callback)
         model = PPO.load("./eval_logs/" + num + "/" + str(i) + "/" + "best_model")
-        mean_reward, std_reward = evaluate_policy(
-            model, eval_env, deterministic=True, n_eval_episodes=25
-        )
+        mean_reward, std_reward = evaluate_policy(model, eval_env, deterministic=True, n_eval_episodes=25)
         print(mean_reward)
         print(std_reward)
         all_mean_rewards.append(mean_reward)
         if mean_reward > 90:
-            model.save(
-                "./mature_policies/"
-                + str(num)
-                + "/"
-                + str(i)
-                + "_"
-                + str(mean_reward).split(".")[0]
-                + ".zip"
-            )
+            model.save("./mature_policies/" + str(num) + "/" + str(i) + "_" + str(mean_reward).split(".")[0] + ".zip")
     except:
         print("Error occurred during evaluation")
 
--- render.py
+++ render.py
@@ -1,10 +1,11 @@
-from stable_baselines3 import PPO
-from pettingzoo.butterfly import pistonball_v6
-import supersuit as ss
-import numpy as np
 import os
 import sys
+
+import numpy as np
+import supersuit as ss
 from array2gif import write_gif
+from pettingzoo.butterfly import pistonball_v6
+from stable_baselines3 import PPO
 
 os.environ["SDL_VIDEODRIVER"] = "dummy"
 num = sys.argv[1]
@@ -22,7 +23,7 @@ n_agents = 20
 for policy in policies:
     model = PPO.load("./mature_policies/" + str(num) + "/" + policy)
 
-    for j in ['a','b','c','d','e']:
+    for j in ["a", "b", "c", "d", "e"]:
 
         obs_list = []
         i = 0
@@ -32,15 +33,13 @@ for policy in policies:
         while True:
             for agent in env.agent_iter():
                 observation, reward, done, _ = env.last()
-                action = (model.predict(observation, deterministic=False)[0] if not done else None)
+                action = model.predict(observation, deterministic=False)[0] if not done else None
                 total_reward += reward
 
                 env.step(action)
                 i += 1
                 if i % (len(env.possible_agents) + 1) == 0:
-                    obs_list.append(
-                        np.transpose(env.render(mode="rgb_array"), axes=(1, 0, 2))
-                    )
+                    obs_list.append(np.transpose(env.render(mode="rgb_array"), axes=(1, 0, 2)))
 
             break
 
@@ -49,5 +48,7 @@ for policy in policies:
         if total_reward > 90:
             print("writing gif")
             write_gif(
-                obs_list, "./mature_gifs/" + num + "_" + policy.split("_")[0] + j + '_' + str(total_reward)[:5] + ".gif", fps=15
+                obs_list,
+                "./mature_gifs/" + num + "_" + policy.split("_")[0] + j + "_" + str(total_reward)[:5] + ".gif",
+                fps=15,
             )
