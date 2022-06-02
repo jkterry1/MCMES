98c4504e37d6dee36ec3cb87f5ac233bc57a9c26
J K Terry <jkterry0@gmail.com>
2022-06-02 12:25:39 -0400

    linting

--- eval_hyperparameters.py
+++ eval_hyperparameters.py
@@ -1,5 +1,4 @@
 import json
-import pdb
 import sys
 
 import sumo_rl
@@ -8,7 +7,7 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.callbacks import EvalCallback
 from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.preprocessing import is_image_space, is_image_space_channels_first
-from stable_baselines3.common.vec_env import VecMonitor, VecNormalize, VecTransposeImage
+from stable_baselines3.common.vec_env import VecMonitor, VecTransposeImage
 from torch import nn as nn
 
 num = sys.argv[1]
--- render.py
+++ render.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import sys
+
 import imageio
 import sumo_rl
 import supersuit as ss
